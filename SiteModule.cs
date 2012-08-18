18123c9d9a1de8468d10196da6bbd62223c3a9e3
Davy Brion <ralinx@davybrion.com>
2012-08-18 14:02:56 +0200

    obligatory hello world

---
+++ SiteModule.cs
@@ -0,0 +1,12 @@
+﻿using Nancy;
+
+namespace ThatExtraMile.be
+{
+    public class SiteModule : NancyModule
+    {
+        public SiteModule()
+        {
+            Get["/"] = parameters => "Hello world";
+        }
+    }
+}
